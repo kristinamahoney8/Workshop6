efbe61c8b175774560c8b753c168d9ae9f39664b
kristinamahoney8 <kmah94@gmail.com>
2017-04-17 12:24:41 -0400

    node

---
+++ server/src/helloworld.js
@@ -0,0 +1,15 @@
+
+
+
+
+// In './test_importer.js':
+
+// Import 'Foo'
+var Util = require('./util');
+var ReverseString = Util.reverseString;
+// Import the default export of './test_default_module.js' module.
+
+
+
+
+console.log(ReverseString("Hello World!"));
